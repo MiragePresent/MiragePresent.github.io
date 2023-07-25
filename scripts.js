471ac1174056a5e2c0ed5b496bd77a1b3022becc
Davyd Holovii <mirage.present@gmail.com>
2023-07-24 22:57:40 -0400

    Remove everything

--- scripts.js
+++
@@ -1,5 +0,0 @@
-function loadJoke(data) {
-    fetch('https://api.chucknorris.io/jokes/random')
-    .then(response => response.json())
-    .then(response => { data.joke = { text: response.value }});
-}
